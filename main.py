061de41fc595f045fef7213428963fb075cc81a6
Mohamed Hazem Al-sherif <110854944+mohazem-cell@users.noreply.github.com>
2025-11-12 10:37:42 +0000

    Add research chatbot code and SearXNG tool

--- main.py
+++ main.py
@@ -7,13 +7,9 @@ from anthropic import BadRequestError
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
-# from tools import search_tool,wiki_tool,save_tool
-# from tools import read_tool
-# from langchain.agents import create_openai_functions_agent
 from tools import search_tool
 
 load_dotenv()
-
 
 class ResearchResponse(BaseModel):
     topic: str
@@ -21,13 +17,6 @@ class ResearchResponse(BaseModel):
     sources: list[str]
     tools_used: list[str] 
 
-# llm=ChatOpenAI(model="gpt-4o-mini")
-
-# llm2 = ChatOpenAI(
-#     model="deepseek-chat",
-#     api_key=os.getenv("DEEPSEEK_API_KEY"),
-#     base_url="https://openrouter.ai/api/v1",
-# )
 api_key = os.getenv("GROQ_API_KEY")
 if not api_key:
     raise ValueError("❌ GROQ_API_KEY not found. Please set it in your .env file or environment variables.")
@@ -40,24 +29,10 @@ llm2 = ChatOpenAI(
     temperature=0
 )
 
-# print("Loaded key:", os.getenv("ANTHROPIC_API_KEY"))  # check if it's loaded
-
 parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 
-response = llm2.invoke("What is the capital of France?")
-print(response.content)
-
-# prompt = ChatPromptTemplate.from_messages([
-#     ("system", """
-#         You are a research assistent that will help generate a research paper. 
-#         Answer the user query and use neccessary tools.
-#         Wrap the output in this format and provide no other text:
-#         \n{format_instructions}.
-#     """),
-#     ("placeholder", "{chat_history}"),
-#     ("human", "{Query}"),
-#     ("placeholder", "{agent_scratchpad}")
-# ]).partial(format_instructions=parser.get_format_instructions())
+# response = llm2.invoke("What is the capital of France?")
+# print(response.content)
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", """
@@ -77,7 +52,6 @@ agent = create_tool_calling_agent(
     llm=llm2,
     tools=tools,
     prompt=prompt
-    # output_parser=parser
 )
 
 agent_executor = AgentExecutor.from_agent_and_tools(
@@ -101,5 +75,3 @@ while True:
     except Exception as e:
         print("Error parsing response:", e, "Raw response",raw_response)
 
-# raw_response = agent_executor.invoke({"Query": "Provide a detailed search about Egyptian history."})
-# print("Raw response:", raw_response)
